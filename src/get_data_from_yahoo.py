2e88540aec0e588646c69fe19dd8351261c430e1
furka <furkantaskin@std.iyte.edu.tr>
2023-11-09 22:06:52 +0300

    Removing the unnecessary indicators

--- src/get_data_from_yahoo.py
+++ src/get_data_from_yahoo.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import yfinance as yf
 import pandas_ta as ta
+import warnings
+
+# Ignore FutureWarnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 period = "1y"
 interval = "1d"
 # Define the ticker symbols
 ndxt_symbols = ['AAPL', 'MSFT', 'AMZN', 'GOOGL', 'GOOG', 'TSLA', 'NVDA']
 
-df_all = pd.DataFrame(
-    columns=['Stock', 'Open', 'High', 'Low', 'Close', 'Volume', 'SMA200', 'SMA100', 'SMA20', 'RSI', 'MACD', 'STOCH'])
-
+df_all = pd.DataFrame(columns = ['Stock', 'Open', 'High', 'Low', 'Close', 'Volume', 'SMA150', 'SMA100', 'SMA20', 'RSI'])
 for ticker in ndxt_symbols:
     # Download the data from Yahoo Finance for the ticker and specified period
     data = yf.download(ticker, period=period, interval=interval)
@@ -17,25 +19,19 @@ for ticker in ndxt_symbols:
     # Remove this month's data
     data = data[data.index.month != pd.Timestamp.now().month]
 
-    # Calculate the 100-day and 20-day SMA for the stock using pandas
-    sma200 = data["Close"].rolling(200).mean()
+    # Calculate the 150-day, 100-day and 20-day SMA for the stock using pandas
+    sma150 = data["Close"].rolling(150).mean()
     sma100 = data["Close"].rolling(100).mean()
     sma20 = data["Close"].rolling(20).mean()
 
     # Calculate the RSI for the stock using pandas_ta
     rsi = ta.rsi(data["Close"], length=14)
 
-    # Calculate the MACD for the stock using pandas_ta
-    macd = ta.macd(data["Close"])
-
-    # Calculate the Stochastic Oscillator for the stock using pandas_ta
-    stoch = ta.stoch(data["High"], data["Low"], data["Close"])
 
     # Create a pandas DataFrame with the data
-    df = pd.DataFrame(
-        {'Stock': ticker, 'Open': data['Open'], 'High': data['High'], 'Low': data['Low'], 'Close': data['Close'],
-         'Volume': data['Volume'], 'SMA200': sma200, 'SMA100': sma100, 'SMA20': sma20, 'RSI': rsi, 'MACD': macd, 'STOCH': stoch})
-    df_all = df_all.append(df)
+    df = pd.DataFrame({'Stock': ticker,'Open': data['Open'],'High': data['High'],'Low': data['Low'],'Close': data['Close'],
+                       'Volume': data['Volume'],'SMA150': sma150, 'SMA100': sma100, 'SMA20': sma20, 'RSI': rsi})
+    df_all = df_all._append(df)
 
 # Rename the index column to "Date"
 df_all = df_all.rename_axis("Date")
